ac35a39fb31aeaed8e3dc692a7f6680f078e4598
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-06-25 17:46:08 +0430

    feat:rate calculator functions for createODG

--- src/odg.js
+++ src/odg.js
@@ -2,6 +2,7 @@ const DepGraph = require('dependency-graph').DepGraph;
 const _ = require('lodash');
 const chalk = require('chalk');
 const Initializer = require('./initializer');
+const stringSimilarity = require('string-similarity');
 
 class ODGInitializer extends Initializer {
   constructor(...props) {
@@ -32,39 +33,78 @@ class ODGInitializer extends Initializer {
   }
 
   smartParseParameters(
-    currentMethodName,
+    currentMethod,
     currentPathMethods,
     dependencies,
     currentPath,
     paths
   ) {
     // create raw result and add parameters keys
-    const inputParams = this.parseParameters(
-      currentMethodName,
-      currentPathMethods
-    );
+    const inputParams = this.parseParameters(currentMethod, currentPathMethods);
 
-    if (currentPathMethods[currentMethodName]) {
+    if (currentPathMethods[currentMethod]) {
       // meta data that helps us to get the right dependencies among apis
-      const tags = currentPathMethods[currentMethodName]?.tags;
-      const operationId = currentPathMethods[currentMethodName]?.operationId;
-      const keys = Object.keys(inputParams);
+      const tags = currentPathMethods[currentMethod]?.tags;
 
-      // logging the values
-      console.log(
-        `${currentPath} >> ${currentMethodName}`,
-        tags,
-        operationId,
-        keys
+      // get keys for each parameter type
+      const currentRequestBodykeys = this.objectKeysArray(
+        inputParams.requestBody
+      );
+      const currentUrlParamskeys = this.objectKeysArray(inputParams.urlParams);
+      const currentQueryParamskeys = this.objectKeysArray(
+        inputParams.queryParams
+      );
+      const currentHeaderParamskeys = this.objectKeysArray(
+        inputParams.headerParams
       );
 
+      // logging the values
+      console.log(`CURRENT : ${currentPath} >> ${currentMethod}`, tags);
+      console.log(`REQUEST BODY : ${currentRequestBodykeys}`);
+      console.log(`URL PARAMS : ${currentUrlParamskeys}`);
+      console.log(`QUERY PARAMS : ${currentQueryParamskeys}`);
+      console.log(`HEADER PARAMS : ${currentHeaderParamskeys}`);
       // searching other apis for similarities
-      for (const path in paths) {
-        // not considering the current path
-        if (path === currentPath) {
-          continue;
+      for (const targetPath in paths) {
+        for (const targetMethod in paths[targetPath]) {
+          // first take the response format of the target
+          const response = this.parseResponse(paths, targetPath, targetMethod);
+          // getting target tags for comparison
+          const targetTags = paths[targetPath][targetMethod]?.tags;
+          // do not consider current path for processing and no response apis
+          if (
+            (targetPath === currentPath && targetMethod === currentMethod) ||
+            !response
+          ) {
+            continue;
+          } else {
+            const responseKeys = this.objectKeysArray(response);
+            console.log(
+              `TARGET : ${targetPath} >> ${targetMethod}`,
+              responseKeys,
+              targetTags
+            );
+
+            // then we should apply our similarity algorithm
+            // between each parameter keys and response keys
+            // but before we continue, we measure two other
+            // values , tags intersection rate and url substring rate
+
+            // reqBody of current vs Response
+            // urlParams of current vs Response
+            // queryParams vs Response
+
+            if (currentRequestBodykeys) {
+              // check request Body
+            } else if (currentUrlParamskeys) {
+              // check url params
+            } else if (currentQueryParamskeys) {
+              // check query params
+            } else if (currentHeaderParamskeys) {
+              // check header params
+            }
+          }
         }
-        //TODO **Complete This Part** ---> processing the other paths
       }
     }
 
@@ -72,36 +112,88 @@ class ODGInitializer extends Initializer {
     return inputParams;
   }
 
-  parseParameters(currentMethodName, currentPathMethods, dependencies) {
+  parseParameters(currentMethod, currentPathMethods, dependencies) {
     dependencies = [];
 
-    if (!currentPathMethods[currentMethodName]) {
+    if (!currentPathMethods[currentMethod]) {
       return null;
     }
 
     const inputParams = {
       requestBody: this.onlyKeysObjectFromObject(
-        currentPathMethods[currentMethodName]?.requestBody?.content[
+        currentPathMethods[currentMethod]?.requestBody?.content[
           'application/json'
         ]?.schema?.properties
       ),
       urlParams: this.onlyKeysObjectFromArray(
-        currentPathMethods[currentMethodName]?.parameters
+        currentPathMethods[currentMethod]?.parameters
           ?.filter((item) => item.in === 'path')
           ?.map((item) => item.name)
       ),
       queryParams: this.onlyKeysObjectFromArray(
-        currentPathMethods[currentMethodName]?.parameters
+        currentPathMethods[currentMethod]?.parameters
           ?.filter((item) => item.in === 'query')
           ?.map((item) => item.name)
       ),
       headerParams: this.onlyKeysObjectFromArray(
-        currentPathMethods[currentMethodName]?.parameters
+        currentPathMethods[currentMethod]?.parameters
           ?.filter((item) => item.in === 'header')
           ?.map((item) => item.name)
       ),
     };
     return inputParams;
+  }
+
+  makeSelector(fieldCoef = 0.7, tagCoef = 0.1, urlCoef = 0.2) {
+    const selector = (
+      fieldSimilarityRate = 0,
+      commonTagsRate = 0,
+      relatedURLRate = 0
+    ) =>
+      fieldCoef * fieldSimilarityRate +
+      tagCoef * commonTagsRate +
+      urlCoef * relatedURLRate;
+    return selector;
+  }
+
+  commonTagsRate(tags1 = [], tags2 = []) {
+    const allTags = _.union(tags1, tags2);
+    const commonTags = _.intersection(tags1, tags2);
+
+    if (allTags.length === 0 || commonTags.length === 0) {
+      return 0;
+    }
+
+    return commonTags.length / allTags.length;
+  }
+
+  relatedURLRate(url1, url2) {
+    const first = _.includes(url1, url2);
+    const second = _.includes(url2, url1);
+    if (first || second) {
+      const rate = stringSimilarity.compareTwoStrings(url1, url2);
+      return rate >= 50 ? rate / 100 : 0.5;
+    } else {
+      const rate = stringSimilarity.compareTwoStrings(url1, url2);
+      return rate;
+    }
+  }
+
+  parseResponse(paths, targetPath, targetMethod) {
+    const objectResult =
+      paths[targetPath][targetMethod]?.responses[200]?.content[
+        'application/json'
+      ].schema?.properties;
+    const arrayResult =
+      paths[targetPath][targetMethod]?.responses[200]?.content[
+        'application/json'
+      ].schema?.items?.properties;
+
+    return objectResult || arrayResult;
+  }
+
+  objectKeysArray(object) {
+    return object ? Object.keys(object) : null;
   }
 
   onlyKeysObjectFromObject(object) {
